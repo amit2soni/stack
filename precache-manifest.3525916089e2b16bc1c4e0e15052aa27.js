55d06b10d8d179f1066f7e10811ca45d2f5b72e0
amit2soni <100334959+amit2soni@users.noreply.github.com>
2022-10-03 22:34:35 +0530

    Updates

---
+++ precache-manifest.3525916089e2b16bc1c4e0e15052aa27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a96e29217863ee6448a",
+    "url": "/static/css/main.f2612621.chunk.css"
+  },
+  {
+    "revision": "3a96e29217863ee6448a",
+    "url": "/static/js/main.3a96e292.chunk.js"
+  },
+  {
+    "revision": "17fb29ed40cd8c1083bf",
+    "url": "/static/js/1.17fb29ed.chunk.js"
+  },
+  {
+    "revision": "162d0f0b9e26ff0c2fdd",
+    "url": "/static/js/2.162d0f0b.chunk.js"
+  },
+  {
+    "revision": "6911db604fedf1b3b856",
+    "url": "/static/js/runtime~main.6911db60.js"
+  },
+  {
+    "revision": "33612edd6e53653b36f5410e1dd9ef55",
+    "url": "/index.html"
+  }
+];
